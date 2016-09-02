4028c28c70f2d6ce9d77c8bab2120668442ac439
travolque@gmail.com <travolque@gmail.com>
2016-09-02 19:37:55 +0200

    added SSL and TLS support to JavaEmail

--- src/com/vangav/backend/networks/email/JavaEmailInl.java
+++ src/com/vangav/backend/networks/email/JavaEmailInl.java
@@ -28,15 +28,17 @@
 
 package com.vangav.backend.networks.email;
 
-import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import com.vangav.backend.exceptions.CodeException;
+import com.vangav.backend.exceptions.VangavException.ExceptionClass;
 import com.vangav.backend.exceptions.VangavException.ExceptionType;
 import com.vangav.backend.exceptions.handlers.ArgumentsInl;
 
@@ -52,29 +54,39 @@ public class JavaEmailInl {
 
   // disable default instantiation
   private JavaEmailInl () {}
-
+  
+  /**
+   * Enum SslType
+   */
+  public enum SslType {
+    
+    WITHOUT_SSL,
+    WITH_SSL
+  }
+  
   /**
    * sendEmail
-   * sends an e-mail
-   * Maps for email receivers has pairs of (email, name)
-   * @param fromEmail
    * @param fromName
-   * @param toEmails
-   * @param ccEmails - can be null
-   * @param bccEmails - can be null
+   * @param fromEmail
+   * @param toEmails (comma separated e-mail addresses)
+   * @param ccEmails can be null - (comma separated e-mail addresses)
+   * @param bccEmails can be null - (comma separated e-mail addresses)
    * @param subject
-   * @param body
+   * @param bodyText
+   * @param sslType
    * @throws Exception
    */
   public static void sendEmail (
-    String fromEmail,
     String fromName,
-    Map<String, String> toEmails,
-    Map<String, String> ccEmails,
-    Map<String, String> bccEmails,
+    String fromEmail,
+    String toEmails,
+    String ccEmails,
+    String bccEmails,
     String subject,
-    String body) throws Exception {
+    String bodyText,
+    SslType sslType) throws Exception {
     
+    // check arguments
     ArgumentsInl.checkNotEmpty(
       "from-email",
       fromEmail,
@@ -93,56 +105,136 @@ public class JavaEmailInl {
       ExceptionType.CODE_EXCEPTION);
     ArgumentsInl.checkNotNull(
       "body",
-      body,
+      bodyText,
       ExceptionType.CODE_EXCEPTION);
-      
+    
+    // set session properties
+
     Properties props = new Properties();
-    Session session = Session.getDefaultInstance(props, null);
 
-    Message msg = new MimeMessage(session);
+    if (sslType == SslType.WITHOUT_SSL) {
 
-    msg.setFrom(
-      new InternetAddress(
-        fromEmail,
-        fromName) );
-
-    for (Map.Entry<String, String> entry : toEmails.entrySet() ) {
-  
-      msg.addRecipient(
-        Message.RecipientType.TO,
-        new InternetAddress(
-          entry.getKey(),
-          entry.getValue() ) );
-    }
-
-    if (ccEmails != null) {
+      props.put(
+        "mail.smtp.host",
+        JavaEmailProperties.i().getStringPropterty(
+          JavaEmailProperties.kSmtpHost));
+      props.put(
+        "mail.smtp.starttls.enable",
+        "true");
+      props.put(
+        "mail.smtp.auth",
+        "true");
+      props.put(
+        "mail.smtp.port",
+        JavaEmailProperties.i().getStringPropterty(
+          JavaEmailProperties.kSmtpNoSslPort) );
+    } else if (sslType == SslType.WITH_SSL) {
       
-      for (Map.Entry<String, String> entry : ccEmails.entrySet() ) {
-    
-        msg.addRecipient(
-          Message.RecipientType.CC,
-          new InternetAddress(
-            entry.getKey(),
-            entry.getValue() ) );
-      }
-    }
-
-    if (bccEmails != null) {
+      props.put(
+        "mail.smtp.host",
+        JavaEmailProperties.i().getStringPropterty(
+          JavaEmailProperties.kSmtpHost) );
+      props.put(
+        "mail.smtp.socketFactory.port",
+        JavaEmailProperties.i().getStringPropterty(
+          JavaEmailProperties.kSmtpSslPort) );
+      props.put(
+        "mail.smtp.socketFactory.class",
+        "javax.net.ssl.SSLSocketFactory");
+      props.put(
+        "mail.smtp.auth",
+        "true");
+      props.put(
+        "mail.smtp.port",
+        JavaEmailProperties.i().getStringPropterty(
+          JavaEmailProperties.kSmtpSslPort) );
+    } else {
       
-      for (Map.Entry<String, String> entry : bccEmails.entrySet() ) {
-    
-        msg.addRecipient(
-          Message.RecipientType.BCC,
-          new InternetAddress(
-            entry.getKey(),
-            entry.getValue() ) );
-      }
+      throw new CodeException(
+        "Unknown SslType ["
+          + sslType.toString()
+          + "] exepcting WITH_SSL or WITHOUT_SSL",
+        ExceptionClass.TYPE);
     }
+    
+    // create session
+    
+    Session session = null;
 
-    msg.setSubject(subject);
+    if (sslType == SslType.WITHOUT_SSL) {
 
-    msg.setText(body);
+      session =
+        Session.getInstance(
+          props,
+          new javax.mail.Authenticator() {
+        
+            protected PasswordAuthentication getPasswordAuthentication() {
+          
+              try {
+                
+                return
+                  new PasswordAuthentication(
+                    JavaEmailProperties.i().getStringPropterty(
+                      JavaEmailProperties.kSmtpUserName),
+                    JavaEmailProperties.i().getStringPropterty(
+                      JavaEmailProperties.kSmtpPassword) );
+              } catch (Exception e) {
+                
+                return null;
+              }
+            }
+          }
+        );
+    } else if (sslType == SslType.WITH_SSL) {
+     
+      session =
+        Session.getDefaultInstance(
+          props,
+          new javax.mail.Authenticator() {
+            
+            protected PasswordAuthentication getPasswordAuthentication() {
+          
+              try {
+              
+                return
+                  new PasswordAuthentication(
+                    JavaEmailProperties.i().getStringPropterty(
+                      JavaEmailProperties.kSmtpUserName),
+                    JavaEmailProperties.i().getStringPropterty(
+                      JavaEmailProperties.kSmtpPassword) );
+              } catch (Exception e) {
+                
+                return null;
+              }
+            }
+          }
+        );
+    }
+    
+    // create message
+    
+    Message message = new MimeMessage(session);
+    
+    message.setFrom(new InternetAddress(fromEmail, fromName) );
+    message.addRecipients(
+      Message.RecipientType.TO,
+      InternetAddress.parse(toEmails) );
+    if (ccEmails != null && ccEmails.length() > 0) {
 
-    Transport.send(msg);
+      message.addRecipients(
+        Message.RecipientType.CC,
+        InternetAddress.parse(ccEmails) );
+    }
+    if (bccEmails != null && bccEmails.length() > 0) {
+
+      message.addRecipients(
+        Message.RecipientType.BCC,
+        InternetAddress.parse(bccEmails) );
+    }
+    message.setSubject(subject);
+    message.setText(bodyText);
+
+    // send email
+    Transport.send(message);
   }
 }
