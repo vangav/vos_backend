5e047b92fa1326a9f07f738adeb86c2b800040f2
travolque@gmail.com <travolque@gmail.com>
2016-10-29 15:44:27 +0200

    Updated from no-license to MIT license.

--- src/com/vangav/backend/play_framework/controllers_generator/ControllersGeneratorInl.java
+++ src/com/vangav/backend/play_framework/controllers_generator/ControllersGeneratorInl.java
@@ -5,8 +5,27 @@
  * */
 
 /**
- * no license, I know you already got more than enough to worry about
- * keep going, never give up
+ * MIT License
+ *
+ * Copyright (c) 2016 Vangav
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  * */
 
 /**
@@ -355,8 +374,27 @@ public class ControllersGeneratorInl {
     + " * */\n\n"
     
     + "/**\n"
-    + " * no license, I know you already got more than enough to worry about\n"
-    + " * keep going, never give up\n"
+    + " * MIT License\n"
+    + " *\n"
+    + " * Copyright (c) 2016 Vangav\n"
+    + " *\n"
+    + " * Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    + " * of this software and associated documentation files (the \"Software\"), to\n"
+    + " * deal in the Software without restriction, including without limitation the\n"
+    + " * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or\n"
+    + " * sell copies of the Software, and to permit persons to whom the Software is\n"
+    + " * furnished to do so, subject to the following conditions:\n"
+    + " *\n"
+    + " * The above copyright notice and this permission notice shall be included in\n"
+    + " * all copies or substantial portions of the Software.\n"
+    + " *\n"
+    + " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    + " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    + " * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    + " * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    + " * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
+    + " * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n"
+    + " * IN THE SOFTWARE.\n"
     + " * */\n\n"
     
     + "/**\n"
@@ -649,8 +687,27 @@ public class ControllersGeneratorInl {
     + " * */\n\n"
     
     + "/**\n"
-    + " * no license, I know you already got more than enough to worry about\n"
-    + " * keep going, never give up\n"
+    + " * MIT License\n"
+    + " *\n"
+    + " * Copyright (c) 2016 Vangav\n"
+    + " *\n"
+    + " * Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    + " * of this software and associated documentation files (the \"Software\"), to\n"
+    + " * deal in the Software without restriction, including without limitation the\n"
+    + " * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or\n"
+    + " * sell copies of the Software, and to permit persons to whom the Software is\n"
+    + " * furnished to do so, subject to the following conditions:\n"
+    + " *\n"
+    + " * The above copyright notice and this permission notice shall be included in\n"
+    + " * all copies or substantial portions of the Software.\n"
+    + " *\n"
+    + " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    + " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    + " * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    + " * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    + " * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
+    + " * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n"
+    + " * IN THE SOFTWARE.\n"
     + " * */\n\n"
     
     + "/**\n"
@@ -774,8 +831,27 @@ public class ControllersGeneratorInl {
     + " * */\n\n"
     
     + "/**\n"
-    + " * no license, I know you already got more than enough to worry about\n"
-    + " * keep going, never give up\n"
+    + " * MIT License\n"
+    + " *\n"
+    + " * Copyright (c) 2016 Vangav\n"
+    + " *\n"
+    + " * Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    + " * of this software and associated documentation files (the \"Software\"), to\n"
+    + " * deal in the Software without restriction, including without limitation the\n"
+    + " * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or\n"
+    + " * sell copies of the Software, and to permit persons to whom the Software is\n"
+    + " * furnished to do so, subject to the following conditions:\n"
+    + " *\n"
+    + " * The above copyright notice and this permission notice shall be included in\n"
+    + " * all copies or substantial portions of the Software.\n"
+    + " *\n"
+    + " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    + " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    + " * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    + " * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    + " * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
+    + " * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n"
+    + " * IN THE SOFTWARE.\n"
     + " * */\n\n"
     
     + "/**\n"
@@ -933,8 +1009,27 @@ public class ControllersGeneratorInl {
     + " * */\n\n"
     
     + "/**\n"
-    + " * no license, I know you already got more than enough to worry about\n"
-    + " * keep going, never give up\n"
+    + " * MIT License\n"
+    + " *\n"
+    + " * Copyright (c) 2016 Vangav\n"
+    + " *\n"
+    + " * Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    + " * of this software and associated documentation files (the \"Software\"), to\n"
+    + " * deal in the Software without restriction, including without limitation the\n"
+    + " * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or\n"
+    + " * sell copies of the Software, and to permit persons to whom the Software is\n"
+    + " * furnished to do so, subject to the following conditions:\n"
+    + " *\n"
+    + " * The above copyright notice and this permission notice shall be included in\n"
+    + " * all copies or substantial portions of the Software.\n"
+    + " *\n"
+    + " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    + " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    + " * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    + " * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    + " * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
+    + " * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n"
+    + " * IN THE SOFTWARE.\n"
     + " * */\n\n"
     
     + "/**\n"
@@ -1203,8 +1298,27 @@ public class ControllersGeneratorInl {
     + " * */\n\n"
     
     + "/**\n"
-    + " * no license, I know you already got more than enough to worry about\n"
-    + " * keep going, never give up\n"
+    + " * MIT License\n"
+    + " *\n"
+    + " * Copyright (c) 2016 Vangav\n"
+    + " *\n"
+    + " * Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    + " * of this software and associated documentation files (the \"Software\"), to\n"
+    + " * deal in the Software without restriction, including without limitation the\n"
+    + " * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or\n"
+    + " * sell copies of the Software, and to permit persons to whom the Software is\n"
+    + " * furnished to do so, subject to the following conditions:\n"
+    + " *\n"
+    + " * The above copyright notice and this permission notice shall be included in\n"
+    + " * all copies or substantial portions of the Software.\n"
+    + " *\n"
+    + " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    + " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    + " * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    + " * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    + " * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
+    + " * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS\n"
+    + " * IN THE SOFTWARE.\n"
     + " * */\n\n"
     
     + "/**\n"
